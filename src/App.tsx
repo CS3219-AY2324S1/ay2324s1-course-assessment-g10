b10672a87235cc2c32c5146e80d73771f4112bb9
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-09-29 01:54:11 +0800

    Fix imports for HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import  HomePage  from "./pages/HomePage/HomePage";
 import Add from "./pages/AddQuestionPage/AddQuestion";
-import React from 'react';
 
 
 function App() {
@@ -11,7 +9,7 @@ function App() {
     <div className="app">
       <Router>
         <Routes>
-          <Route exact path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path = "/create" element={<Add />} />
         </Routes>
       </Router>
