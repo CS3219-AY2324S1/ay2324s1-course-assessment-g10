88377797635bc941e534297f9851fe49ce35ea63
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-09-29 01:40:46 +0800

    Migrate App.js to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Add from "./pages/AddQuestionPage/AddQuestion";
+import React from 'react';
 
 
 function App() {
