15b8e60e39faf6e6aa44f53dc9505e2914b46039
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-10-01 20:26:28 +0800

    Add fetchQuestion, updateQuestion, deleteQuestion
    Add the following methods to the question service REST API

--- question_service/controller/questionController.js
+++ question_service/controller/questionController.js
@@ -10,14 +10,35 @@ const fetchAllQuestions = async (req, res) => {
 
 }
 
+//@desc     fetch a question
+//@route    GET /api/questions/:id
+//@access   authenticated users
+const fetchQuestion = async (req, res) => {
+    try {
+        // function provided by mongoose to find an Question document with a given ID
+        // req.params.id is retrieved from /:id in route
+        const question = await Question.findById(req.params.id)
+
+        res.status(200).json({
+            _id: question._id,
+            title: question.title,
+            description: question.description,
+            category: question.category,
+            complexity: question.complexity
+        })
+    } catch (error) {
+        res.status(400).json({ message: 'Invalid ID. Question not found in database.' })
+    }
+}
+
 //@desc     add a question
 //@route    POST /api/questions
 //@access   admin only
 const addQuestion = async (req, res) => {
-    const {title, description, category, complexity} = req.body
+    const { title, description, category, complexity } = req.body
 
     if (!title || !description || !category || !complexity) {
-        return res.status(400).json({message: 'Please enter all fields'})
+        return res.status(400).json({ message: 'Please enter all fields' })
     }
 
     try {
@@ -33,9 +54,62 @@ const addQuestion = async (req, res) => {
             complexity: question.complexity
         })
     } catch (error) {
-        res.status(400).json({message: 'Invalid question data'})
+        res.status(400).json({ message: 'Invalid question data' })
+    }
+}
+
+// @desc    Update a question
+// @route   PUT /api/addresses/:id
+// @access  admin only
+const updateQuestion = async (req, res) => {
+    const { title, description, category, complexity } = req.body
+
+    if (!title || !description || !category || !complexity) {
+        return res.status(400).json({ message: 'Please enter all fields' })
+    }
+
+    try {
+        // function provided by mongoose to find an Question document with a given ID
+        // req.params.id is retrieved from /:id in route
+        const question = await Question.findById(req.params.id)
+        // update the document
+        question.title = title
+        question.description = description
+        question.category = category
+        question.complexity = complexity
+        // function provided by mongoose to
+        // save the changes made to a document
+        await question.save()
+        // return the updated address in JSON format
+        // with success status 200
+        res.status(200).json({
+            _id: question._id,
+            title: question.title,
+            description: question.description,
+            category: question.category,
+            complexity: question.complexity
+        })
+    } catch (error) {
+        res.status(400).json({ message: 'Invalid question data.' })
+    }
+}
+       
+
+
+// @desc    Delete a question
+// @route   DELETE /api/addresses/:id
+// @access  admin only
+const deleteQuestion = async (req, res) => {
+    try {
+        // function provided by mongoose to find an Question document with a given ID
+        // req.params.id is retrieved from /:id in route
+        const question = await Question.findById(req.params.id);
+        await question.deleteOne();
+        res.status(200).json({ message: 'Question removed' });
+    } catch (error) {
+        res.status(404).json({ message: 'Question not found' })
     }
 }
 
 
-module.exports = { fetchAllQuestions, addQuestion }
+module.exports = { fetchAllQuestions, fetchQuestion, addQuestion, updateQuestion, deleteQuestion }
--- question_service/routes/questionRouter.js
+++ question_service/routes/questionRouter.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { fetchAllQuestions, addQuestion } = require('../controller/questionController');
+const { fetchAllQuestions, addQuestion, updateQuestion, deleteQuestion, fetchQuestion } = require('../controller/questionController');
 
 router.route('/').get(fetchAllQuestions)
 router.route('/').post(addQuestion)
+router.route('/:id').get(fetchQuestion)
+router.route('/:id').put(updateQuestion)
+router.route('/:id').delete(updateQuestion)
+
 
 module.exports = router
