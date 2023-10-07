9c225120574bf16858aeebd0c14367cf3a3ced41
Gavzzz <gavinchowb@gmail.com>
2023-10-07 18:02:33 +0800

    Restructured file and and fixed errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import  HomePage  from "./pages/HomePage/HomePage";
 import Add from "./pages/AddQuestionPage/AddQuestion";
+import { Provider } from 'react-redux';
+
+
 
 
 function App() {
