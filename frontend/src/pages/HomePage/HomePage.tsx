e2ac2c97beb906e8345261aa0cc066d55ecd4b79
Isaaclhy00 <97405333+Isaaclhy00@users.noreply.github.com>
2023-10-13 01:18:14 +0800

    Revert "Updated frontend ui and add question functions"

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,62 +1,47 @@
-import React, { useState, useEffect } from "react";
-import "./HomePage.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Link, useNavigate } from "react-router-dom";
-import {
-  Question,
-  initializeData,
-  QuestionData,
-} from "../AddQuestionPage/QuestionData";
+import React, { useState, useEffect } from 'react';
+import './HomePage.css'; 
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Link, useNavigate} from 'react-router-dom';
+import { Question, initializeData, QuestionData }  from '../AddQuestionPage/QuestionData';
 
 function HomePage() {
+
   let history = useNavigate();
 
   const [QuestionData, setQuestionData] = useState(initializeData());
 
   const handleDelete = (id: number) => {
-    const confirmed = window.confirm(
-      "Are you sure you want to delete this question?"
-    );
-
-    if (confirmed) {
-      // Find the index of the question to delete in the QuestionData array
-      const dataIndex = QuestionData.findIndex(
-        (item: Question) => item.id === id
-      );
-
-      // Find the index of the question to delete in the filteredData array
-      const filteredIndex = filteredData.findIndex(
-        (item: Question) => item.id === id
-      );
-
-      if (dataIndex !== -1) {
-        // Remove the question from the QuestionData array
-        QuestionData.splice(dataIndex, 1);
-      }
-
-      if (filteredIndex !== -1) {
-        // Remove the question from the filteredData array
-        filteredData.splice(filteredIndex, 1);
-      }
-
-      // Update local storage with the modified QuestionData
-      localStorage.setItem("QuestionData", JSON.stringify(QuestionData));
-
-      // Navigate back to the homepage
-      history("/");
-    } else {
-      // User canceled, do nothing
+    // Find the index of the question to delete in the QuestionData array
+    const dataIndex = QuestionData.findIndex((item: Question) => item.id === id);
+  
+    // Find the index of the question to delete in the filteredData array
+    const filteredIndex = filteredData.findIndex((item: Question) => item.id === id);
+  
+    if (dataIndex !== -1) {
+      // Remove the question from the QuestionData array
+      QuestionData.splice(dataIndex, 1);
     }
-  };
+  
+    if (filteredIndex !== -1) {
+      // Remove the question from the filteredData array
+      filteredData.splice(filteredIndex, 1);
+    }
+  
+    // Update local storage with the modified QuestionData
+    localStorage.setItem('QuestionData', JSON.stringify(QuestionData));
+  
+    // Navigate back to the homepage
+    history('/');
+  }
 
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState('');
   const [filteredData, setFilteredData] = useState(QuestionData);
 
   const handleSearchInputChange = (event: any) => {
     const query = event.target.value;
     setSearchQuery(query);
 
-    if (query === "") {
+    if (query === '') {
       // If the search query is empty, revert to showing the original data
       setFilteredData(QuestionData);
       return;
@@ -66,7 +51,7 @@ function HomePage() {
     const filtered = QuestionData.filter((item: Question) =>
       Object.values(item).some((value) => {
         // Check if the value is a string before converting to lowercase
-        if (typeof value === "string") {
+        if (typeof value === 'string') {
           return value.toLowerCase().includes(query.toLowerCase());
         }
 
@@ -78,25 +63,25 @@ function HomePage() {
     // Set the new filtered data
     setFilteredData(filtered);
   };
-
+  
   return (
     <div className="homepage">
       <div className="background" />
 
       <div className="navbar">
-        <div className="title">Peer Prep</div>
+        <div className="title">Question Bank</div>
       </div>
 
       <div className="content-container">
         <input
           type="text"
           className="search-bar"
-          placeholder="Search questions"
+          placeholder="Search Question Here"
           value={searchQuery}
           onChange={handleSearchInputChange}
         />
         <div className="add-question-button">
-          <Link to="/create" className="add-question-text">
+          <Link to="/create" className='add-question-text'>
             Add Question
           </Link>
         </div>
@@ -120,40 +105,10 @@ function HomePage() {
               <td>{item.title}</td>
               <td>{item.description}</td>
               <td>{item.category}</td>
+              <td>{item.complexity}</td>
               <td>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center", // Horizontally center the box
-                    alignItems: "center", // Vertically center the box
-                  }}
-                >
-                  <div
-                    style={{
-                      backgroundColor:
-                        item.complexity === "1"
-                          ? "rgba(0, 255, 0, 0.3)"
-                          : item.complexity === "2"
-                          ? "rgba(255, 255, 0, 0.3)"
-                          : "rgba(255, 0, 0, 0.3)",
-                      padding: "4px",
-                      borderRadius: "5px",
-                      width: "30px", // Set the width here
-                      textAlign: "center",
-                      fontWeight: "bold",
-                      color: "white",
-                    }}
-                  >
-                    {item.complexity}
-                  </div>
-                </div>
-              </td>
-              <td>
-                <button
-                  className="delete-button"
-                  onClick={() => handleDelete(item.id)}
-                >
-                  <i className="trash-icon"></i>
+                <button className="delete-button" onClick={() => handleDelete(item.id)}>
+                  <i className="trash-icon"  ></i>
                 </button>
               </td>
             </tr>
@@ -163,5 +118,6 @@ function HomePage() {
     </div>
   );
 }
+
 
 export default HomePage;
