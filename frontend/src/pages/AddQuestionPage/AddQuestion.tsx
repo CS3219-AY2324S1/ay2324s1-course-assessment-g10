410125913a074d256dbe02df97a2ed32f096cc31
Gavzzz <gavinchowb@gmail.com>
2023-10-13 00:48:33 +0800

    fixed indentation and added chakra package

--- frontend/src/pages/AddQuestionPage/AddQuestion.tsx
+++ frontend/src/pages/AddQuestionPage/AddQuestion.tsx
@@ -1,24 +1,23 @@
-import React, {useState} from 'react';
-import {Button, Form} from 'react-bootstrap'
-import "bootstrap/dist/css/bootstrap.min.css"
-import {useNavigate} from 'react-router-dom';
-import { Question, initializeData, QuestionData } from './QuestionData';
-import './AddQuestion.css'; 
-import '../HomePage/HomePage.css'; 
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
+import { Question, initializeData, QuestionData } from "./QuestionData";
+import "./AddQuestion.css";
+import "../HomePage/HomePage.css";
 // import '../App.css'
 
 function Add() {
-
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [category, setCategory] = useState('');
-  const [complexity, setComplexity] = useState('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [complexity, setComplexity] = useState("");
   const [QuestionData, setQuestionData] = useState(initializeData());
 
-    let history = useNavigate();
+  let history = useNavigate();
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
 
     // Find the maximum ID in the existing QuestionData
     const maxId = Math.max(...QuestionData.map((item: Question) => item.id), 0);
@@ -28,79 +27,104 @@ function Add() {
 
     // Create a new question object with the new ID and the title provided by the user
     const newQuestion = {
-        id: newId,
-        title: title,
-        description: description,
-        category: category,
-        complexity: complexity,
-      };
-   
+      id: newId,
+      title: title,
+      description: description,
+      category: category,
+      complexity: complexity,
+    };
+
     // Add the new question to the QuestionData array
     const updatedData = [...QuestionData, newQuestion];
     setQuestionData(updatedData);
 
     // Store the updated data in local storage
-    localStorage.setItem('QuestionData', JSON.stringify(updatedData));
+    localStorage.setItem("QuestionData", JSON.stringify(updatedData));
 
     // Navigate back to the homepage
     history("/");
-  
-    }
+  };
 
-    const handleCancel = (e: any) => {
-        e.preventDefault();
+  const handleCancel = (e: any) => {
+    e.preventDefault();
 
-        history("/");
-  
-    }
+    history("/");
+  };
 
+  return (
+    <div className="homepage">
+      <div className="background" />
 
-    return (
-        <div className="homepage">
-          <div className="background" />
-    
-          <div className="navbar">
-            <div className="title">Peer Prep</div>
-          </div>
-    
-            <Form className="d-grid gap-2" style={{margin:"8rem auto", width: "80%", alignItems: "center"}}>
+      <div className="navbar">
+        <div className="title">Peer Prep</div>
+      </div>
 
-                <Form.Group className="mb-3" controlId="formName">
-                    <Form.Control type="text" placeholder = "Enter Question Title" required onChange= {(e) => setTitle(e.target.value)}>
-                    </Form.Control>
-                </Form.Group>
+      <Form
+        className="d-grid gap-2"
+        style={{ margin: "8rem auto", width: "80%", alignItems: "center" }}
+      >
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Control
+            type="text"
+            placeholder="Enter Question Title"
+            required
+            onChange={(e) => setTitle(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formComplexity">
-                    <Form.Control as="select" value={complexity} onChange={(e) => setComplexity(e.target.value)}>
-                    <option value="">Select Complexity</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    </Form.Control>
-                    </Form.Group>
+        <Form.Group className="mb-3" controlId="formComplexity">
+          <Form.Control
+            as="select"
+            value={complexity}
+            onChange={(e) => setComplexity(e.target.value)}
+          >
+            <option value="">Select Complexity</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </Form.Control>
+        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formName">
-                    <Form.Control type="text" placeholder = "Enter Question Category" required onChange= {(e) => setCategory(e.target.value)}>
-                    </Form.Control>
-                </Form.Group>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Control
+            type="text"
+            placeholder="Enter Question Category"
+            required
+            onChange={(e) => setCategory(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formName">
-                <Form.Control
-                    as="textarea"  
-                    style={{ height: '200px' }}  
-                    placeholder="Enter Question Description"
-                    required
-                    onChange={(e) => setDescription(e.target.value)}
-                        />
-                </Form.Group>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Control
+            as="textarea"
+            style={{ height: "200px" }}
+            placeholder="Enter Question Description"
+            required
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </Form.Group>
 
-                <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-                    <Button onClick={(e) => handleCancel(e)} type ="submit" style={{width: "30%"}}> Cancel </Button>
-                    <Button onClick={(e) => handleSubmit(e)} type ="submit" style={{width: "30%"}}> Submit</Button> 
-                </div>
-            </Form>
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <Button
+            onClick={(e) => handleCancel(e)}
+            type="submit"
+            style={{ width: "30%" }}
+          >
+            {" "}
+            Cancel{" "}
+          </Button>
+          <Button
+            onClick={(e) => handleSubmit(e)}
+            type="submit"
+            style={{ width: "30%" }}
+          >
+            {" "}
+            Submit
+          </Button>
         </div>
-      );
+      </Form>
+    </div>
+  );
 }
 
 export default Add;
--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,55 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import './HomePage.css'; 
-import "bootstrap/dist/css/bootstrap.min.css"
-import {Link, useNavigate} from 'react-router-dom';
-import { Question, initializeData, QuestionData }  from '../AddQuestionPage/QuestionData';
+import React, { useState, useEffect } from "react";
+import "./HomePage.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Question,
+  initializeData,
+  QuestionData,
+} from "../AddQuestionPage/QuestionData";
 
 function HomePage() {
-
   let history = useNavigate();
 
   const [QuestionData, setQuestionData] = useState(initializeData());
 
   const handleDelete = (id: number) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this question?"
+    );
 
-    const confirmed = window.confirm('Are you sure you want to delete this question?');
+    if (confirmed) {
+      // Find the index of the question to delete in the QuestionData array
+      const dataIndex = QuestionData.findIndex(
+        (item: Question) => item.id === id
+      );
 
-  if (confirmed) {
+      // Find the index of the question to delete in the filteredData array
+      const filteredIndex = filteredData.findIndex(
+        (item: Question) => item.id === id
+      );
 
-    // Find the index of the question to delete in the QuestionData array
-    const dataIndex = QuestionData.findIndex((item: Question) => item.id === id);
-  
-    // Find the index of the question to delete in the filteredData array
-    const filteredIndex = filteredData.findIndex((item: Question) => item.id === id);
-  
-    if (dataIndex !== -1) {
-      // Remove the question from the QuestionData array
-      QuestionData.splice(dataIndex, 1);
+      if (dataIndex !== -1) {
+        // Remove the question from the QuestionData array
+        QuestionData.splice(dataIndex, 1);
+      }
+
+      if (filteredIndex !== -1) {
+        // Remove the question from the filteredData array
+        filteredData.splice(filteredIndex, 1);
+      }
+
+      // Update local storage with the modified QuestionData
+      localStorage.setItem("QuestionData", JSON.stringify(QuestionData));
+
+      // Navigate back to the homepage
+      history("/");
+    } else {
+      // User canceled, do nothing
     }
-  
-    if (filteredIndex !== -1) {
-      // Remove the question from the filteredData array
-      filteredData.splice(filteredIndex, 1);
-    }
-  
-    // Update local storage with the modified QuestionData
-    localStorage.setItem('QuestionData', JSON.stringify(QuestionData));
-  
-    // Navigate back to the homepage
-    history('/');
-  } else {
-    // User canceled, do nothing
-  }
-  }
+  };
 
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [filteredData, setFilteredData] = useState(QuestionData);
 
   const handleSearchInputChange = (event: any) => {
     const query = event.target.value;
     setSearchQuery(query);
 
-    if (query === '') {
+    if (query === "") {
       // If the search query is empty, revert to showing the original data
       setFilteredData(QuestionData);
       return;
@@ -59,7 +66,7 @@ function HomePage() {
     const filtered = QuestionData.filter((item: Question) =>
       Object.values(item).some((value) => {
         // Check if the value is a string before converting to lowercase
-        if (typeof value === 'string') {
+        if (typeof value === "string") {
           return value.toLowerCase().includes(query.toLowerCase());
         }
 
@@ -71,7 +78,7 @@ function HomePage() {
     // Set the new filtered data
     setFilteredData(filtered);
   };
-  
+
   return (
     <div className="homepage">
       <div className="background" />
@@ -89,12 +96,11 @@ function HomePage() {
           onChange={handleSearchInputChange}
         />
         <div className="add-question-button">
-          <Link to="/create" className='add-question-text'>
+          <Link to="/create" className="add-question-text">
             Add Question
           </Link>
         </div>
       </div>
-
 
       <table className="question-table">
         <thead>
@@ -115,29 +121,39 @@ function HomePage() {
               <td>{item.description}</td>
               <td>{item.category}</td>
               <td>
-              <div style={{
-                display: 'flex',
-                  justifyContent: 'center', // Horizontally center the box
-                  alignItems: 'center', // Vertically center the box
-                    }}>
-                  <div style={{
-                  backgroundColor: item.complexity === '1' ? 'rgba(0, 255, 0, 0.3)' :
-                        item.complexity === '2' ? 'rgba(255, 255, 0, 0.3)' :
-                        'rgba(255, 0, 0, 0.3)',
-                      padding: '4px',
-                  borderRadius: '5px',
-                  width: '30px', // Set the width here
-                  textAlign: 'center',
-                fontWeight: 'bold',
-                color: 'white'
-              }}>
-          {item.complexity}
-        </div>
-      </div>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center", // Horizontally center the box
+                    alignItems: "center", // Vertically center the box
+                  }}
+                >
+                  <div
+                    style={{
+                      backgroundColor:
+                        item.complexity === "1"
+                          ? "rgba(0, 255, 0, 0.3)"
+                          : item.complexity === "2"
+                          ? "rgba(255, 255, 0, 0.3)"
+                          : "rgba(255, 0, 0, 0.3)",
+                      padding: "4px",
+                      borderRadius: "5px",
+                      width: "30px", // Set the width here
+                      textAlign: "center",
+                      fontWeight: "bold",
+                      color: "white",
+                    }}
+                  >
+                    {item.complexity}
+                  </div>
+                </div>
               </td>
               <td>
-                <button className="delete-button" onClick={() => handleDelete(item.id)}>
-                  <i className="trash-icon"  ></i>
+                <button
+                  className="delete-button"
+                  onClick={() => handleDelete(item.id)}
+                >
+                  <i className="trash-icon"></i>
                 </button>
               </td>
             </tr>
@@ -147,6 +163,5 @@ function HomePage() {
     </div>
   );
 }
-
 
 export default HomePage;
