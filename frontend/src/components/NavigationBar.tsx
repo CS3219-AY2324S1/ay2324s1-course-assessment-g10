0da0ff397bc18b9f2bf5e9b27830735106e74bc1
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-10-16 02:07:24 +0800

    Add LogoutButton
    Clicking on the logout button will clear the user from the redux store,
    thereby setting the page to an unauthenticated state. For now, the
    logout button is accessible via the NavigationBar when the user is
    logged in

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,7 +1,14 @@
+import { useSelector } from "react-redux";
+import { selectIsAuthenticated } from "../reducers/authSlice"
+import LogoutButton from "./auth/LogoutButton";
+
 export default function NavigationBar() {
+
+    const isAuthenticated = useSelector(selectIsAuthenticated);
 
     return (
     <div className="navbar">
         <div className="title">Peer Prep</div>
+        { isAuthenticated ? <LogoutButton/> : (<div/>) }
     </div>)
 }
