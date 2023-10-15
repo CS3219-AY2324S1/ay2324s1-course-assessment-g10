db7c067b3d1385dd03f30ebc2e8df23333361af0
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-10-16 01:40:38 +0800

    Initialize redux store and chakra provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './reducers/store'
+import { ChakraProvider } from '@chakra-ui/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>,
 );
 
