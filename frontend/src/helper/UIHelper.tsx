71311c7a0c0b49579701f6e1decf58967e6ab7fd
ZQ <e0725180@u.nus.edu>
2023-10-21 10:47:10 +0800

    Adds helper method to color difficulty

---
+++ frontend/src/helper/UIHelper.tsx
@@ -0,0 +1,16 @@
+/// maps difficulty value to appropriate colors
+export const diffToScheme = (diff: number) => {
+  if (diff < 3) {
+    return "green";
+  }
+  if (diff < 6) {
+    return "yellow";
+  }
+  if (diff < 8) {
+    return "orange";
+  }
+  return "red";
+};
+
+export const isLoggedin = true;
+export const isAdmin = true;
