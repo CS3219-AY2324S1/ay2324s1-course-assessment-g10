59dfa2a1b06cb480b15b5ddc8f99278300e7d517
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-10-21 19:18:25 +0800

    Enable persistent login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Navigate } from 'react-router-dom';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import Add from "./pages/AddQuestionPage/AddQuestion";
 import { useSelector } from 'react-redux';
-import { selectIsAuthenticated, selectUser } from './reducers/authSlice'
+import { selectIsAuthenticated, selectUser, setUser } from './reducers/authSlice'
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
+import { useDispatch } from 'react-redux';
+import { getSessionUser } from './api/auth';
+import { useToast } from '@chakra-ui/toast';
 
+let firstLoad = true;
 
 function App() {
 
   const isAuthenticated = useSelector(selectIsAuthenticated);
   const user = useSelector(selectUser)
+  const dispatch = useDispatch();
+  const toast = useToast();
+
+  /**
+   * Attempt to load the previously logged in user if their cookie still exists
+   */
+  useEffect(() => {
+    console.log(`first load: ${firstLoad}`);
+
+    if (firstLoad) {
+      firstLoad = false;
+      getSessionUser()
+        .then((fetchedUser) => {
+          console.log(fetchedUser)
+
+          if (!fetchedUser) {
+            console.log('no user fetched!');
+          } else {
+            dispatch(setUser(fetchedUser));
+          }
+        })
+        .catch((err) => {
+          toast({
+            description: 'Please login again',
+            status: 'warning',
+            isClosable: true,
+          });
+
+          console.error(err.message);
+        });
+    }
+  }, []);
 
   console.log(user)
   console.log(isAuthenticated)
--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,5 +1,11 @@
 import { userServiceClient } from "./server"
 
+/**
+ * Makes a login request and sets the access_token if successful
+ * 
+ * @param username 
+ * @param password 
+ */
 export async function login(username: String, password: String) {
     const response = await userServiceClient.post('/login', {
         username: username,
@@ -9,6 +15,13 @@ export async function login(username: String, password: String) {
     return response;
 }
 
+
+/**
+ * Registers the user and sets the access_token if successful
+ * 
+ * @param username 
+ * @param password 
+ */
 export async function register(username: String, password: String) {
     const response = await userServiceClient.post('/register', {
         username: username,
@@ -16,4 +29,21 @@ export async function register(username: String, password: String) {
     })
 
     return response;
+}
+
+/**
+ * Gets the current session user using the httpOnly cookie
+ * 
+ * @returns the user if succesfully authenticated, else the error response
+ * @side_effects A failure to authenticate the JWT token will cause the server side 
+ * to set the AUTH_SESSION cookie on the frontend to null
+ */
+export async function getSessionUser() {
+    const response = await userServiceClient.get('/getSessionUser');
+    
+    try {
+        return response.data.user;
+    } catch (error) {
+        return response
+    }
 }
