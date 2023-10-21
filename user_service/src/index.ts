20e6b2d34dc61921ddf14d2121ba4aa49529659a
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-10-21 19:08:49 +0800

    Set cors options to accept credentials

--- user_service/src/index.ts
+++ user_service/src/index.ts
@@ -7,9 +7,15 @@ import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 8081;
+const corsOptions ={
+    origin:'http://localhost:3000', //TODO: need to add our production url here once we host it. Currently assuming all requests will be made from this url
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
 
 app.use(cookieParser());
-app.use(cors<Request>());
+app.use(cors<Request>(corsOptions));
 app.use(express.json());
 
 app.use('/api/users', userRouter);
