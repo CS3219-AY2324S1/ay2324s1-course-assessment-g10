21e1aed9502b1bd8300ec0c5844289d4e2a7756d
ZQ <e0725180@u.nus.edu>
2023-10-23 00:02:57 +0800

    Add markdowneditor component

--- frontend/src/components/MarkdownEditor/MarkdownEditor.component.tsx
+++ frontend/src/components/MarkdownEditor/MarkdownEditor.component.tsx
@@ -1,46 +1,120 @@
-import { useLayoutEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
 import { languages } from "@codemirror/language-data";
-
-import { Box, HStack } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  useDisclosure,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
+  Card,
+  CardHeader,
+  Flex,
+  IconButton,
+  Spacer,
+  Text,
+  Center,
+} from "@chakra-ui/react";
+import { EditIcon } from "@chakra-ui/icons";
 import { MarkdownViewer } from "../MarkdownVIewer/MarkdownViewer";
+import { CardBody } from "react-bootstrap";
 
-export const MarkdownEditor = () => {
-  const [code, setCode] = useState("");
-  const [height, setHeight] = useState(0);
+type MarkdownEditorProp = {
+  title?: string;
+  markdown?: string;
+  onSave?: (markdown: string) => void;
+};
+
+export const MarkdownEditor = (prop: MarkdownEditorProp) => {
+  const [code, setCode] = useState(prop.markdown ?? "");
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const ref = useRef<HTMLDivElement>(null);
 
-  useLayoutEffect(() => {
-    const h = (ref.current?.offsetHeight ?? 400) - 40;
-    setHeight(h); // this is super scuffed cos 100% doesnt work
-  }, []);
-
   return (
-    <HStack spacing="2" width="100%" height="100%">
-      <Box width="60%" height="100%" padding="20px" overflowY="auto" ref={ref}>
-        <CodeMirror
-          value={code}
-          height={`${height}px`}
-          extensions={[
-            markdown({
-              base: markdownLanguage,
-              codeLanguages: languages,
-            }),
-          ]}
-          onChange={(value) => setCode(value)}
-        />
-      </Box>
-      <Box
-        width="40%"
+    <>
+      <Card
+        width="100%"
         height="100%"
         padding="20px"
         overflowY="auto"
-        boxShadow="xl"
+        variant="outline"
+        size="md"
       >
-        <MarkdownViewer markdown={code} />
-      </Box>
-    </HStack>
+        <CardHeader>
+          <Flex alignItems="center">
+            <Text fontSize="xl" fontWeight="bold">
+              {prop.title ?? "Description"}
+            </Text>
+            <Spacer />
+            <IconButton
+              variant="ghost"
+              colorScheme="gray"
+              aria-label="Edit Markdown"
+              icon={<EditIcon />}
+              onClick={onOpen}
+            />
+          </Flex>
+        </CardHeader>
+        <CardBody>
+          <Box padding="4" height="100%" overflow="auto">
+            {code === "" ? (
+              <Center>
+                <Text>No detail have being provided</Text>
+              </Center>
+            ) : (
+              <MarkdownViewer markdown={code} />
+            )}
+          </Box>
+        </CardBody>
+      </Card>
+
+      <Modal isOpen={isOpen} onClose={onClose} size="6xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalBody>
+            <HStack
+              spacing="2"
+              width="100%"
+              height="100%"
+              paddingTop="2"
+              paddingBottom="2"
+            >
+              <Box width="60%" height="80vh" ref={ref} boxShadow="md">
+                <CodeMirror
+                  value={code}
+                  height="calc(80vh)"
+                  extensions={[
+                    markdown({
+                      base: markdownLanguage,
+                      codeLanguages: languages,
+                    }),
+                  ]}
+                  onChange={(value) => setCode(value)}
+                />
+              </Box>
+              <Box
+                width="40%"
+                height="80vh"
+                padding="20px"
+                overflowY="auto"
+                boxShadow="md"
+              >
+                {code === "" ? (
+                  <Center>
+                    <Text>No detail have being provided</Text>
+                  </Center>
+                ) : (
+                  <MarkdownViewer markdown={code} />
+                )}
+              </Box>
+            </HStack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
