4ae94aaee4a212c5960b553aa16ee36f4fd91321
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-10-24 14:17:25 +0800

    Catch error on logout fail

--- frontend/src/components/auth/LogoutButton.tsx
+++ frontend/src/components/auth/LogoutButton.tsx
@@ -1,14 +1,23 @@
-import { Button } from "@chakra-ui/react"
+import { Button, useToast } from "@chakra-ui/react"
 import { clearUser } from '../../reducers/authSlice';
 import { useDispatch } from 'react-redux';
 import { logOut } from "../../api/auth";
 
 export default function LogoutButton() {
     const dispatch = useDispatch();
+    const toast = useToast();
 
     const handleLogout = async () => {
-        await logOut();
-        dispatch(clearUser());
+        try {
+            const response = await logOut();
+            dispatch(clearUser());
+        } catch (err : any) {
+            toast({
+                title: 'Failed to logout',
+                description: err.msg,
+                status: 'error'
+            })
+        }
     }
 
     return (
