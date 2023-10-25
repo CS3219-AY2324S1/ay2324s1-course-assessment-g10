e357cbe68963c7d14fc7d7ac851c0c1f5b577712
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-10-26 00:34:36 +0800

    Integrate editQuestion with BE's updateQuestion

--- frontend/src/api/questions.ts
+++ frontend/src/api/questions.ts
@@ -52,4 +52,33 @@ export async function delQuestion(id : number) {
 }
 
 
+/**
+ * Updates a question on the backend server via PUT 
+ * 
+ * @param _id The uuid identifying the question resource
+ * @param question The new question info to add to the database
+ * @returns
+ */
+export async function updateQuestion(_id: string, question: Question) {
+    const response : AxiosResponse = await questionServiceClient.put(`/api/questions/${_id}`, {
+        title: question.title,
+        description: question.descMd,
+        topics: question.topics,
+        difficulty: question.difficulty
+    })
+
+    const resData = response.data;
+    const resQuestion : Question = new Question(
+        resData._id, 
+        0, //BACKEND DOES NOT RETURN AN ID FIELD YET
+        resData.title, 
+        resData.description, 
+        resData.topics, 
+        resData.difficulty
+    );
+
+    return resQuestion;
+
+}
+
 
