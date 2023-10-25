1878f6100a5e027daa921da4f81582792ba1a97b
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-10-25 22:33:01 +0800

    Integrate fetchAllQuestions w backend

--- frontend/src/pages/BankPage/Bank.page.tsx
+++ frontend/src/pages/BankPage/Bank.page.tsx
@@ -1,29 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { QnTable } from "../../components/QnTable/QnTable.component";
 import { FilterBar } from "../../components/QnFilter/QnFilter.component";
-import { VStack } from "@chakra-ui/react";
+import { VStack, useToast } from "@chakra-ui/react";
 import { QnFilter } from "../../models/Question.model";
 import { loadQuestions } from "../../data/sampleqn";
 import { useDispatch, useSelector } from "react-redux";
 import { selectFilteredQuestions, setQuestions } from "../../reducers/questionsSlice";
 import { RootState } from "../../reducers/store";
 import { fetchAllQuestions } from "../../api/questions";
+import { AxiosResponse } from "axios";
 
 const BankPage = () => {
   const [filter, setFilter] = useState<QnFilter>({});
   const filteredQns = useSelector((state: RootState) =>selectFilteredQuestions(state, filter));
   const dispatch = useDispatch();
+  const toast = useToast();
 
   useEffect(() => {
-    console.log('dispatching...')
-    loadQuestions();
-    fetchAllQuestions().then((res) => {
-      const questions = res.data.questions;
-      dispatch(setQuestions(questions));
-    }).catch((err) => {
-      console.log(err.message);
-    })
-    // setTimeout(() => dispatch(setQuestions(dummyQn)), 3000) //simulating network fetch
+    console.log('fetching...')
+    
+    if (process.env.ENV_TYPE !== 'prod') {
+      loadQuestions();
+    } else {      
+      fetchAllQuestions().then((res : AxiosResponse) => {
+        const questions = res.data;
+        dispatch(setQuestions(questions));
+      }).catch((err) => {
+        console.log(err.message);
+        toast({
+          title: 'Error',
+          description: err.message,
+          status: 'error'
+        })
+      })
+    }
   })
 
   return (
