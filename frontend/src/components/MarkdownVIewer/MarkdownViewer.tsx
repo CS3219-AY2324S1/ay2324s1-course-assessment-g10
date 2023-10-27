9d4f4a7818481ef263fd04b0acadfa6487fb93b6
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-10-27 21:36:50 +0800

    Allow MD viewer to load image from base64
    Previously, MarkdownViewer is aggressively sanitizing our data url using
    the `defaultUrlTransform` function. Lets provide a custom transform
    function to prevent it from sanitizing our dataurl

--- frontend/src/components/MarkdownVIewer/MarkdownViewer.tsx
+++ frontend/src/components/MarkdownVIewer/MarkdownViewer.tsx
@@ -1,11 +1,19 @@
-import Markdown from "react-markdown";
+import Markdown, { defaultUrlTransform } from "react-markdown";
 import remarkGfm from "remark-gfm";
 import remarkBreaks from "remark-breaks";
 import remarkMath from "remark-math";
 
+function urlTransformer(url: string) {
+  if (/^data:image\/(png|jpeg);base64,/.test(url)) {
+    return url;
+  }
+
+  return defaultUrlTransform(url);
+}
+
 export const MarkdownViewer = ({ markdown }: { markdown: string }) => {
   return (
-    <Markdown remarkPlugins={[remarkGfm, remarkBreaks, remarkMath]}>
+    <Markdown remarkPlugins={[remarkGfm, remarkBreaks, remarkMath]} skipHtml={false} urlTransform={urlTransformer}>
       {markdown}
     </Markdown>
   );
