5e1a833e02afd221cf00d76be2f66a6d64144f4c
ZQ <e0725180@u.nus.edu>
2023-10-29 07:15:00 +0800

    Remove collabeditor from homepage

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import "./HomePage.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import CollabEditor from "../../components/CollabEditor/CollabEditor.component";
 
 function HomePage() {
-  const [code, setCode] = useState("");
-  return (
-    <CollabEditor
-      language="C++"
-      onChange={(s) => setCode(s)}
-      defaultValue="hello"
-    ></CollabEditor>
-  );
+  return <></>;
 }
 
 export default HomePage;
