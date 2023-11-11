b081cb3582ca2f6ee1f065cd1a87dccdb104dc1d
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-11-11 11:21:48 +0800

    remove unused files

--- code_execution/test.js
+++
@@ -1,5 +0,0 @@
-const { execute } = require('./execute_code');
-const { language_id, source_code, testCases } = require('./input_sample');
-
-// Example usage
-execute(language_id, source_code, testCases);
