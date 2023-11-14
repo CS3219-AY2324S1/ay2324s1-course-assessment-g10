b8948de030c33cf7b99271855405dbb2c5a98d08
ZQ <e0725180@u.nus.edu>
2023-11-15 05:57:18 +0800

    ws test

--- frontend/src/api/gateway.ts
+++ frontend/src/api/gateway.ts
@@ -7,4 +7,5 @@ export const apiGatewayClient = axios.create({
 
 
 export const wsMatchMakeURL = process.env.NODE_ENV === 'production'? 'http://peerprep-g10.com:7999' : 'http://localhost:7999'
-export const wsCollabUrl = process.env.NODE_ENV === 'production'? 'ws://peerprep-g10.com:7998' :'ws://localhost:7998'
+export const wsCollabUrl = process.env.NODE_ENV === 'production'? 'ws://peerprep-g10.com:8083' :'ws://localhost:7998'
+// export const wsCollabUrl = process.env.NODE_ENV === 'production'? 'ws://peerprep-g10.com:7998' :'ws://localhost:7998'
