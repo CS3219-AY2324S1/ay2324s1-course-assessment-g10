652abf0944c53014b25b34176a60e6dc9a96866d
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-11-17 14:26:06 +0800

    revert user authorization

--- question_service/src/middleware/isAdmin.ts
+++ question_service/src/middleware/isAdmin.ts
@@ -3,7 +3,7 @@ export const isAdmin = (req: any, res: any, next: any) => {
     if (req.auth && req.auth.role === 'ADMIN') {
         next();
     } else {
-        console.log(`${req.auth.username} with role ${req.auth.role} tried to access Admin route`)
+        console.log(`${req.auth.username} with role ${req.auth.role} tried to access Admin route`);
         return res.status(403).json({ message: 'Admin privileges required' });
     }
 
