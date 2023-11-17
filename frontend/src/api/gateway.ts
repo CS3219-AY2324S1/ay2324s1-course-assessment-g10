210cccaf349154dc2782e6f714c5811445cff3db
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-11-17 12:31:17 +0800

    Remove more user stuff

--- frontend/src/api/gateway.ts
+++ frontend/src/api/gateway.ts
@@ -1,4 +1,3 @@
 import axios from 'axios'
 
 export const qnServiceClient = axios.create({ baseURL: 'http://localhost:8080', withCredentials: true })
-export const userServiceClient = axios.create({ baseURL: 'http://localhost:8081', withCredentials: true })
--- frontend/src/contexts/sharededitor.context.tsx
+++ frontend/src/contexts/sharededitor.context.tsx
@@ -1,44 +1,8 @@
 import React, { createContext, useContext, useEffect, useMemo } from "react";
 import { Question } from "../models/Question.model";
 import { useSelector } from "react-redux";
-import { Navigate, useLoaderData } from "react-router-dom";
-import data from "../data/lang_temps.json";
+import { useLoaderData } from "react-router-dom";
 
-export type language = keyof typeof data;
-
-export const langList = Object.keys(data) as language[];
-export const LangDataMap = data;
-
-export type chatRecord = {
-  nickname: string;
-  msg: string;
-  avatar?: string;
-};
-
-type executionResult =
-  | "Accepted"
-  | "TLE"
-  | "MLE"
-  | "WA"
-  | "Compile Error"
-  | "Runtime Error"
-  | "Unknown";
-
-export type SubmissionResult = {
-  evaluated: number;
-  total: number;
-  verdict: string;
-  completed: boolean;
-};
-
-export type submissionRecord = {
-  time: number;
-  userIds: number[];
-  qn_id: string;
-  code: string;
-  lang: language;
-  result: executionResult;
-};
 
 interface SharedEditorInterface {
   qn?: Question;
