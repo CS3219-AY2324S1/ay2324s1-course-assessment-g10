296ae63c24a2629f46266feb08a06da0a714ef28
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-11-17 14:38:16 +0800

    revert user authorization

---
+++ question_service/src/middleware/jwtCheck.ts
@@ -0,0 +1,29 @@
+import { expressjwt } from 'express-jwt';
+
+export const jwtCheckNoCredentials = expressjwt({
+    secret: process.env.SECRET_KEY || 'YOUR_SECRET_KEY',
+    algorithms: ['HS256'],
+    getToken: (req: any) => {
+        return req.cookies['AUTH_SESSION'] ? req.cookies['AUTH_SESSION'] : null
+    },
+    credentialsRequired: false,
+})
+
+export const jwtCheckRequireCredentials = expressjwt({
+    secret: process.env.SECRET_KEY || 'YOUR_SECRET_KEY',
+    algorithms: ['HS256'],
+    getToken: (req: any) => {
+        return req.cookies['AUTH_SESSION'] ? req.cookies['AUTH_SESSION'] : null
+    },
+    credentialsRequired: true,
+})
+
+export function handleAuthFailure(err: any, req: any, res: any, next: any) {
+    if (err.name === 'UnauthorizedError') {
+        res.clearCookie('AUTH_SESSION');
+        res.status(401).send({ message: err.message });
+        return;
+    }
+
+    next(err);
+}
--- question_service/src/routes/questionRouter.ts
+++ question_service/src/routes/questionRouter.ts
@@ -7,14 +7,15 @@ import {
   fetchQuestion,
   fetchARandomQuestion,
 } from "../controller/questionController";
+import { isAdmin } from "../middleware/isAdmin";
 
 const router = express.Router();
 
 router.get("/", fetchAllQuestions);
-router.post("/", addQuestion);
+router.post("/", isAdmin, addQuestion);
 router.get("/:id", fetchQuestion);
-router.put("/:id", updateQuestion);
-router.delete("/:id", deleteQuestion);
+router.put("/:id", isAdmin, updateQuestion);
+router.delete("/:id", isAdmin, deleteQuestion);
 router.post("/random", fetchARandomQuestion);
 
 export default router;
